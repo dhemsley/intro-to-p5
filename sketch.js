d4c62f16b813d93d5821421f7053cb4ba50ed32c
dhemsley <dhemsley@wlapcs.org>
2017-03-02 18:27:55 +0000

    first commit

---
+++ sketch.js
@@ -0,0 +1,25 @@
+function setup(){
+   createCanvas(600,400);
+    background(244, 61, 244);
+}
+
+function draw(){
+        line(35,80,400,200);
+     
+     stroke(255,255,255);
+     fill(131, 12, 210);
+    rect(290,100,250,250);
+    
+    strokeWeight(5); 
+    fill(211, 12, 98);
+    ellipse(50,250,80,80);
+    
+    line(-200,-150,100,400);
+    line(0);
+    
+    rect(12,3,150,200);
+    ellipse(56,300,80,100);
+    line(500,10,500,300);
+    line(80,75,200,75);
+    
+}
